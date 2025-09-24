7dee419c912ef81eb6508f824db8a9fb3b215692
hnazila71 <hnazila71@gmail.com>
2025-09-25 00:33:39 +0700

    .sh

---
+++ run.sh
@@ -0,0 +1,27 @@
+
+#!/bin/bash
+
+# stop kalau ada error
+set -e
+
+
+npm install
+
+
+createdb -U postgres inventory_db || echo "Database inventory_db already exists"
+
+
+if [ ! -f ".env" ]; then
+  echo "Creating .env file..."
+  cp .env.example .env
+else
+  echo ".env file already exists, skipping..."
+fi
+
+
+
+npm run migrate up
+
+
+
+npm start
--- src/infrastructure/database/connection.js
+++ src/infrastructure/database/connection.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
